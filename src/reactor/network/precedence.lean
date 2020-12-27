75e4df70faf4b0566105cfdb204464b7e445583a
marcusrossel <marcusrossel@icloud.com>
2020-12-27 16:50:03 +0100

    Add definitions for precedence things.

--- src/reactor/network/precedence.lean
+++ src/reactor/network/precedence.lean
@@ -6,21 +6,38 @@ namespace reactor
 namespace network
 namespace «precedence» 
 
-  private structure reaction_index {c : ℕ} (n : network c) := 
+  variable  {c : ℕ}
+
+  private structure reaction_index (n : network c) := 
     (rtr : fin c)
     (rcn : fin (n.φ.nodes rtr).nᵣ)
   
-  structure graph.edge {c : ℕ} {n : network c} (_ : reaction_index n → reaction) := 
+  structure graph.edge {n : network c} (_ : reaction_index n → reaction) := 
     (src : reaction_index n)
     (dst : reaction_index n)
 
-  instance prec_graph_edge {c : ℕ} {n : network c} {r : reaction_index n → reaction} : digraph.edge (graph.edge r) reaction := 
+  instance prec_graph_edge {n : network c} {r : reaction_index n → reaction} : digraph.edge (graph.edge r) reaction := 
     {
       src := (λ e, r e.src), 
       dst := (λ e, r e.dst)
     }
 
-  def graph {c : ℕ} (n : network c) := digraph (reaction_index n) reaction graph.edge
+  def graph (n : network c) := digraph (reaction_index n) reaction graph.edge
+
+  namespace graph
+
+    def is_well_formed {n : network c} (g : precedence.graph n) : Prop :=
+      true
+      -- For all reactions that are implicitly connected in a certain way in the network,
+      -- they need to have the analogous explicit connections in the precedence graph.
+
+    def «from» (n : network c) : precedence.graph n := sorry
+
+    theorem from_network_correctness (n : network c) :
+      (precedence.graph.from n).is_well_formed := 
+      sorry
+
+  end graph
 
 end «precedence»
 end network
