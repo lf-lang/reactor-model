ae3844eb240ec6f4745a6cb50f0a65ea71424be2
marcusrossel <marcusrossel@icloud.com>
2020-12-27 19:37:37 +0100

    Add digraph defs.

--- src/digraph.lean
+++ src/digraph.lean
@@ -2,38 +2,73 @@ import data.finset
 
 -- https://leanprover.zulipchat.com/#narrow/stream/113489-new-members/topic/Understanding.20Type.20Classes
 
--- Type `ε` is a `digraph.edge`-type over `α`, if any instance of it can produce a `src` and `dst` element of type `α`.
-class digraph.edge (ε α : Type*) :=
-  (src : ε → α)
-  (dst : ε → α)
+namespace digraph
 
-structure digraph (ι α : Type*) (ε : (ι → α) → Type*) [∀ n, digraph.edge (ε n) α] :=
+  -- Type `ε` is a `digraph.edge`-type over `α`, if any instance of it can produce a `src` and `dst` element of type `α`.
+  class edge (ε α : Type*) :=
+    (src : ε → α)
+    (dst : ε → α)
+
+  namespace edge 
+
+    def src_of {ε α : Type*} [digraph.edge ε α] (e : ε) : α := digraph.edge.src e
+    def dst_of {ε α : Type*} [digraph.edge ε α] (e : ε) : α := digraph.edge.dst e
+
+  end edge
+
+end digraph
+
+structure digraph (ι α : Type*) (ε : (ι → α) → Type*) [∀ n, digraph.edge (ε n) α] [fintype ι] [decidable_eq α] :=
   (nodes : ι → α)
   (edges : finset (ε nodes))
 
-variables {ι α : Type*} {ε : (ι → α) → Type*} [∀ n, digraph.edge (ε n) α]
+variables {ι α : Type*} {ε : (ι → α) → Type*}
+variables [∀ n, digraph.edge (ε n) α] [fintype ι] [decidable_eq α]
 
 namespace digraph
 
-  def has_edge_from_to (g : digraph ι α ε) (a a' : α) : Prop :=
-    ∃ e ∈ g.edges, digraph.edge.src e = a ∧ digraph.edge.dst e = a'
+  open edge
 
-  notation a-g->a' := g.has_edge_from_to a a'
+  -- The values of the nodes contained in digraph `g`.
+  def vertices (g : digraph ι α ε) : finset α :=
+    finset.image g.nodes finset.univ
 
+  -- The proposition that a given digraph connects two given vertices with an edge.
+  def has_edge_from_to (g : digraph ι α ε) (v v' : α) : Prop :=
+    ∃ e ∈ g.edges, (src_of e = v) ∧ (dst_of e = v')
+
+  notation v-g->v' := g.has_edge_from_to v v'
+
+  -- The proposition that a given digraph connects two given vertices by some path.
   inductive has_path_from_to (g : digraph ι α ε) : α → α → Prop
-    | direct {a a' : α} : (a-g->a') → has_path_from_to a a'
-    | composite {a aₘ a' : α} : has_path_from_to a aₘ → has_path_from_to aₘ a' → has_path_from_to a a'
+    | direct {v v' : α} : (v-g->v') → has_path_from_to v v'
+    | composite {v vₘ v' : α} : has_path_from_to v vₘ → has_path_from_to vₘ v' → has_path_from_to v v'
 
   notation a~g~>a' := g.has_path_from_to a a'
 
+  -- The proposition that a given digraph is acyclic.
   def is_acyclic (g : digraph ι α ε) : Prop :=
     ∀ a a' : α, (a~g~>a') → a ≠ a'
 
+  -- The proposition that every node in a given digraph has an in-degree of ≤ 1.
   def is_input_unique (g : digraph ι α ε) : Prop :=
     ∀ (i₁ i₂ i : ι) (e₁ e₂ ∈ g.edges),
-      (digraph.edge.src e₁ = g.nodes i₁) ∧ (digraph.edge.src e₂ = g.nodes i₂) →
-      (digraph.edge.dst e₁ = g.nodes i ) ∧ (digraph.edge.dst e₂ = g.nodes i ) →
+      (src_of e₁ = g.nodes i₁) ∧ (src_of e₂ = g.nodes i₂) →
+      (dst_of e₁ = g.nodes i ) ∧ (dst_of e₂ = g.nodes i ) →
       i₁ = i₂ 
+
+  -- The proposition that `a` is a node of in-degree 0 in `g`.
+  def has_source_node (g : digraph ι α ε) (a : α) : Prop :=
+    a ∈ g.vertices ∧ ∀ e ∈ g.edges, dst_of e ≠ a
+
+  -- If a digraph is acyclic, it must contain a source node.
+  theorem acyclic_has_source (g : digraph ι α ε) :
+    g.is_acyclic → (∃ a : α, g.has_source_node a) :=
+    begin
+      intro h,
+      --? rw has_source_node,
+      sorry
+    end
 
 end digraph 
 
@@ -44,10 +79,12 @@ def list.is_topological_order_of (l : list α) (g : digraph ι α ε)  : Prop :=
   
 namespace digraph
 
-  def topological_sort (g : digraph ι α ε) : list α := sorry
+  private def topological_sort' (g : digraph ι α ε) (acylic : g.is_acyclic)
 
-  protected theorem topological_sort_correctness (g : digraph ι α ε) :
-    (topological_sort g).is_topological_order_of g :=
+  def topological_sort (g : digraph ι α ε) (acylic : g.is_acyclic) : list α := sorry
+
+  protected theorem topological_sort_correctness (g : digraph ι α ε) (acyclic : g.is_acyclic) :
+    (topological_sort g acyclic).is_topological_order_of g :=
     sorry
 
 end digraph
