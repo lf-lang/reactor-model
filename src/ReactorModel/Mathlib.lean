c38620c17de49fffb61d5544dc31a1657cf394c8
Marcus Rossel <marcusrossel@icloud.com>
2021-07-24 16:18:22 +0200

    Resolves #2, resolves #3.

--- src/ReactorModel/Mathlib.lean
+++ src/ReactorModel/Mathlib.lean
@@ -1,61 +1,61 @@
-import Mathlib.Data.List.Basic
+import Mathlib
 
 namespace List
 
 variable (r : α → α → Prop)
 
-inductive Pairwise : List α → Prop
-  | nil : Pairwise []
-  | cons : ∀ {a : α} {l : List α}, (∀ a' ∈ l, r a a') → Pairwise l → Pairwise (a::l)
+inductive pairwise : List α → Prop
+  | nil : pairwise []
+  | cons : ∀ {a : α} {l : List α}, (∀ a' ∈ l, r a a') → pairwise l → pairwise (a::l)
 
-def Nodup : List α → Prop := Pairwise (. ≠ .)
+def nodup : List α → Prop := pairwise (. ≠ .)
 
-inductive Perm : List α → List α → Prop
-  | nil   : Perm [] []
-  | cons  : ∀ (x : α) {l₁ l₂ : List α}, Perm l₁ l₂ → Perm (x::l₁) (x::l₂)
-  | swap  : ∀ (x y : α) (l : List α), Perm (y::x::l) (x::y::l)
-  | trans : ∀ {l₁ l₂ l₃ : List α}, Perm l₁ l₂ → Perm l₂ l₃ → Perm l₁ l₃
+inductive perm : List α → List α → Prop
+  | nil   : perm [] []
+  | cons  : ∀ (x : α) {l₁ l₂ : List α}, perm l₁ l₂ → perm (x::l₁) (x::l₂)
+  | swap  : ∀ (x y : α) (l : List α), perm (y::x::l) (x::y::l)
+  | trans : ∀ {l₁ l₂ l₃ : List α}, perm l₁ l₂ → perm l₂ l₃ → perm l₁ l₃
 
-infix:50 " ~ " => Perm
+infix:50 " ~ " => perm
 
-protected theorem Perm.refl : ∀ (l : List α), l ~ l
-  | []      => Perm.nil
-  | (x::xs) => (Perm.refl xs).cons x
+protected theorem perm.refl : ∀ (l : List α), l ~ l
+  | []      => perm.nil
+  | (x::xs) => (perm.refl xs).cons x
 
-protected theorem Perm.symm {l₁ l₂ : List α} (p : l₁ ~ l₂) : l₂ ~ l₁ := by
+protected theorem perm.symm {l₁ l₂ : List α} (p : l₁ ~ l₂) : l₂ ~ l₁ := by
   induction p with
   | nil => exact nil
   | cons x _ p => exact cons x p
   | swap x y l => exact swap y x l
   | trans _ _ p₁ p₂ => exact trans p₂ p₁
 
-theorem Perm.eqv : Equivalence (@Perm α) where
-  refl := Perm.refl
-  symm := Perm.symm
-  trans := Perm.trans
+theorem perm.eqv : Equivalence (@perm α) where
+  refl := perm.refl
+  symm := perm.symm
+  trans := perm.trans
 
 instance isSetoid (α) : Setoid (List α) :=
-  Setoid.mk _ Perm.eqv
+  Setoid.mk _ perm.eqv
 
-theorem Perm.lengthEq {l₁ l₂ : List α} (p : l₁ ~ l₂) : length l₁ = length l₂ := by
+theorem perm.lengthEq {l₁ l₂ : List α} (p : l₁ ~ l₂) : length l₁ = length l₂ := by
   induction p with
   | nil => rfl
   | cons _ _ h => simp [h]
   | swap => simp
   | trans _ _ h₁ h₂ => exact Eq.trans h₁ h₂
 
-theorem Perm.eqNil {l : List α} (p : l ~ []) : l = [] :=
+theorem perm.eqNil {l : List α} (p : l ~ []) : l = [] :=
   eq_nil_of_length_eq_zero p.lengthEq
 
-theorem Perm.nilEq {l : List α} (p : [] ~ l) : [] = l :=
+theorem perm.nilEq {l : List α} (p : [] ~ l) : [] = l :=
   p.symm.eqNil.symm
 
-theorem Perm.subset {l₁ l₂ : List α} (p : l₁ ~ l₂) : l₁ ⊆ l₂ := by
+theorem perm.subset {l₁ l₂ : List α} (p : l₁ ~ l₂) : l₁ ⊆ l₂ := by
 sorry
 
-theorem Perm.pairwiseIff {R : α → α → Prop} (S : ∀ {x y}, R x y → R y x) :
-  ∀ {l₁ l₂ : List α} (p : l₁ ~ l₂), Pairwise R l₁ ↔ Pairwise R l₂ := by
-  suffices ∀ {l₁ l₂}, l₁ ~ l₂ → Pairwise R l₁ → Pairwise R l₂ 
+theorem perm.pairwiseIff {R : α → α → Prop} (S : ∀ {x y}, R x y → R y x) :
+  ∀ {l₁ l₂ : List α} (p : l₁ ~ l₂), pairwise R l₁ ↔ pairwise R l₂ := by
+  suffices ∀ {l₁ l₂}, l₁ ~ l₂ → pairwise R l₁ → pairwise R l₂ 
   from λ l₁ l₂ p => ⟨this p, this p.symm⟩
   intro l₁ l₂ p d
   induction d generalizing l₂ with
@@ -72,17 +72,73 @@ theorem Perm.pairwiseIff {R : α → α → Prop} (S : ∀ {x y}, R x y → R y 
     exact h _ (p'.symm.subset m)
     -/
 
-theorem Perm.nodupIff {l₁ l₂ : List α} : l₁ ~ l₂ → (List.Nodup l₁ ↔ List.Nodup l₂) :=
-  Perm.pairwiseIff $ Ne.symm
+theorem perm.nodupIff {l₁ l₂ : List α} : l₁ ~ l₂ → (List.nodup l₁ ↔ List.nodup l₂) :=
+  perm.pairwiseIff Ne.symm
+
+theorem perm.memIff {a : α} {l₁ l₂ : List α} (h : l₁ ~ l₂) : a ∈ l₁ ↔ a ∈ l₂ :=
+  Iff.intro (λ m => h.subset m) (λ m => h.symm.subset m)
+
+def keys {β : α → Type v} : List (Sigma β) → List α :=
+  map Sigma.fst
+
+def nodupkeys {β : α → Type v} (l : List (Sigma β)) : Prop :=
+  l.keys.nodup
+
+theorem filterMapConsSome (f : α → Option β) (a : α) (l : List α) {b : β} (h : f a = some b) :
+  filterMap f (a :: l) = b :: filterMap f l :=
+  by simp only [filterMap, h] ; split 
+
+theorem filterMapEqMap (f : α → β) : filterMap (some ∘ f) = map f := by
+  funext l
+  induction l with 
+  | nil => rfl
+  | cons a l IH => 
+    simp only [filterMapConsSome (some ∘ f) _ _ rfl, IH, map_cons]
+    split
+
+theorem perm.filterMap (f : α → Option β) {l₁ l₂ : List α} (p : l₁ ~ l₂) :
+  filterMap f l₁ ~ filterMap f l₂ := sorry
+
+theorem perm.map {β} (f : α → β) {l₁ l₂ : List α} (p : l₁ ~ l₂) :
+  map f l₁ ~ map f l₂ :=
+  filterMapEqMap f ▸ p.filterMap _
+
+theorem permNodupkeys {β : α → Type v} {l₁ l₂ : List (Sigma β)} (h : l₁ ~ l₂) : nodupkeys l₁ ↔ nodupkeys l₂ :=
+  (h.map _).nodupIff
 
 end List
 
 def Multiset.{u} (α : Type u) : Type u :=
   Quotient (List.isSetoid α)
 
-def Multiset.Nodup (s : Multiset α) : Prop :=
-  Quot.liftOn s List.Nodup (λ _ _ p => propext p.nodupIff)
+namespace Multiset
+
+def nodup (s : Multiset α) : Prop :=
+  Quot.liftOn s List.nodup (λ _ _ p => propext p.nodupIff)
+
+def mem (a : α) (s : Multiset α) : Prop :=
+  Quot.liftOn s (λ l => a ∈ l) (λ l₁ l₂ (e : l₁ ~ l₂) => propext e.memIff)
+
+def nodupkeys {β : α → Type v} (s : Multiset (Sigma β)) : Prop :=
+  Quot.liftOn s List.nodupkeys (λ s t p => propext $ List.permNodupkeys p)
+
+instance : Mem α (Multiset α) := ⟨Multiset.mem⟩
+
+end Multiset
 
 structure Finset (α) where
   val : Multiset α
-  nodup : Multiset.Nodup val
+  nodup : Multiset.nodup val
+
+namespace Finset
+
+instance : Mem α (Finset α) := ⟨λ a f => a ∈ f.val⟩
+
+end Finset
+
+structure Finmap {α : Type u} (β : α → Type v) : Type (max u v) where
+  entries : Multiset (Sigma β)
+  nodupkeys : entries.nodupkeys
+
+def Set.finite (s : Set α) : Prop :=
+  ∃ f : Finset α, ∀ a, a ∈ s ↔ a ∈ f
