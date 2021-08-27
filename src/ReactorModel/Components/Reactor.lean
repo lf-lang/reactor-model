f294c67afdfc7b76a7258d5784757a5421b48802
Marcus Rossel <marcusrossel@icloud.com>
2021-08-27 18:19:09 +0200

    Add 'Core Formalization' doc

--- src/ReactorModel/Components/Reactor.lean
+++ src/ReactorModel/Components/Reactor.lean
@@ -10,8 +10,8 @@ def Raw.Reactor.wellFormed (rtr : Raw.Reactor ι υ) : Prop :=
   (∀ m,       m ∈ rtr.muts.values → ∀ i i' s, (i =[m.deps Role.in] i') → m.body i s = m.body i' s) ∧    /-mutsInDepOnly : -/
   (∀ m,       m ∈ rtr.muts.values → ∀ i s o, (o ∉ m.deps Role.out) → (m.body i s).prtVals[o] = none ) ∧ /-mutsOutDepOnly : -/
   (∀ m r,     m ∈ rtr.muts.values → (m.deps r) ⊆ (rtr.ports r).ids) ∧ -- Is this the way it should be?  /-wfMutDeps : -/
-  (∀ rcn r, rcn ∈ rtr.rcns.values → (rcn.deps r) ⊆ (rtr.ports r).ids) ∧                                 /-wfRcnDeps : -/
-  (∀ r : Raw.Reactor ι υ, r ∈ rtr.nest.nodes.values → r.wellFormed)                                     /-nestedWf : -/
+  (∀ rcn r, rcn ∈ rtr.rcns.values → (rcn.deps r) ⊆ (rtr.ports r).ids) -- ∧                                 /-wfRcnDeps : -/
+  -- (∀ r : Raw.Reactor ι υ, r ∈ rtr.nest.nodes.values → r.wellFormed)                                     /-nestedWf : -/
 
 structure Reactor where
   raw : Raw.Reactor ι υ
@@ -23,6 +23,7 @@ namespace Reactor
 
 def ports (rtr : Reactor ι υ) : Ports.Role → Ports ι υ := rtr.raw.ports
 def state (rtr : Reactor ι υ) : StateVars ι υ          := rtr.raw.state
+def rcns  (rtr : Reactor ι υ) : ι ▸ Reaction ι υ       := rtr.raw.rcns
 def prios (rtr : Reactor ι υ) : PartialOrder ι         := rtr.raw.prios
 
 end Reactor
