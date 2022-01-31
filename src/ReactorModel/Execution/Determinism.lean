a6fa2ecc5cfeb1fec7b459818708a25b634b0498
Andres Goens <andres.goens@ed.ac.uk>
2022-01-31 12:54:18 +0000

    minor (syntax/typos)

--- src/ReactorModel/Execution/Determinism.lean
+++ src/ReactorModel/Execution/Determinism.lean
@@ -6,11 +6,11 @@ variable {ι υ} [Value υ]
 -- Primarily, we say the execution is deterministic if there is always ot most one timed
 -- step that can be taken.
 
-lemma Execution.instantaneousConvergent : {σ σ₁ σ₂ : Reactor ι υ} {ctx ctx₁ ctx₂  : Execution.Context ι} : 
+lemma Execution.instantaneousConvergent {σ σ₁ σ₂ : Reactor ι υ} {ctx ctx₁ ctx₂  : Execution.Context ι} :
     (σ, ctx) ⇓ᵢ* (σ₁, ctx₁) →
-    Execution.instantaneousSTuck σ₁ ctx₁ →
+    Execution.instantaneousStuck σ₁ ctx₁ →
     (σ, ctx) ⇓ᵢ* (σ₂, ctx₂) →
-    Execution.instantaneousSTuck σ₂ ctx₂ →
+    Execution.instantaneousStuck σ₂ ctx₂ →
     σ₁ = σ₂ ∧ ctx₁ = ctx₂ := sorry
 
 
