87bd0d85528bf47bde7f24edb7a8f543dd54bf30
Marcus Rossel <marcusrossel@icloud.com>
2022-06-21 18:18:48 +0200

    Update lakefile to new syntax

--- src/lakefile.lean
+++ src/lakefile.lean
@@ -1,11 +1,6 @@
 import Lake
 open Lake DSL
 
-package reactor_model where
-  defaultFacet := PackageFacet.leanLib
-  dependencies := #[
-    {
-      name := `mathlib
-      src := Source.git "https://github.com/leanprover-community/mathlib4.git" "1ac9dea0cdfd0150d1759eef4f76ee0eca5cd108"
-    }
-  ]
+package reactor_model 
+lean_lib ReactorModel
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"1ac9dea0cdfd0150d1759eef4f76ee0eca5cd108"
