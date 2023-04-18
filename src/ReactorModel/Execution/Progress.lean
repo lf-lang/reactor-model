3181918d1910f507e2c45497dab9b7b465e13cae
Marcus Rossel <marcusrossel@icloud.com>
2023-04-18 19:53:56 +0200

    Progress on progress theorem

--- src/ReactorModel/Execution/Progress.lean
+++ src/ReactorModel/Execution/Progress.lean
@@ -30,13 +30,6 @@ theorem clear_cleared (rtr : α) : Cleared rtr (clear rtr) where
 end ReactorType
 
 namespace Execution
-
-theorem Step.not_closed_elim [Practical α] {s₁ s₂ : State α} (e : s₁ ⇓ s₂) (h : ¬s₁.Closed) : 
-    Nonempty (s₁ ⇓| s₂) := by
-  cases e 
-  case advance a => exact absurd a.closed h
-  case close e => exact ⟨e⟩ 
-
 namespace State
 
 variable [Practical α]
@@ -44,8 +37,8 @@ variable [Practical α]
 def unprocessed (s : State α) : Set ID :=
   { i ∈ s.rtr[.rcn] | i ∉ s.progress }
 
-protected structure Over (rtr : α) extends State α where 
-  rtr_eq       : toState.rtr = rtr := by rfl
+protected structure Over (ovr : α) extends State α where 
+  rtr_eq       : rtr = ovr := by rfl
   progress_sub : progress ⊆ rtr[.rcn].ids 
 
 namespace Over
@@ -58,13 +51,11 @@ instance : Coe (State.Over rtr) (State α) where
 theorem progress_finite (s : State.Over rtr) : s.progress.Finite :=
   Finite.fin s.rtr .rcn |>.subset s.progress_sub
 
-theorem unprocessed_finite (s : State.Over rtr) : s.unprocessed.Finite :=
-  sorry
+theorem unprocessed_finite (s : State.Over rtr) : s.unprocessed.Finite := by
+  simp [unprocessed, Set.setOf_and, s.rtr_eq]
+  apply Set.Finite.inter_of_left $ Finite.fin rtr .rcn
 
-def unprocessed' (s : State.Over rtr) : Finset ID :=
-  s.unprocessed_finite.toFinset
-
-theorem unprocessed'_empty_iff_closed : (s.unprocessed' = ∅) ↔ (s.Closed) := by
+theorem unprocessed_empty_iff_closed : (s.unprocessed = ∅) ↔ s.Closed := by
   sorry
 
 theorem not_closed_to_nontrivial (h : ¬s.Closed) : s.Nontrivial := by
@@ -77,6 +68,13 @@ theorem not_closed_to_nontrivial (h : ¬s.Closed) : s.Nontrivial := by
 theorem allowed_of_acyclic_not_closed [State.Nontrivial s.toState] 
     (a : Dependency.Acyclic rtr) (hc : ¬s.Closed) : ∃ i, s.Allows i := 
   sorry -- somehow by induction over the finite set of reactions
+
+def ofStep {s₁ : State.Over rtr₁} {s₂ : State α} (e : s₁.toState ⇓ᵢ s₂) : State.Over s₂.rtr := 
+  { s₂ with 
+    progress_sub := by 
+      simp [e.progress_eq, ←Equivalent.obj?_rcn_eq e.equiv]
+      exact Set.insert_subset.mpr ⟨e.allows_rcn.mem, s₁.progress_sub⟩ 
+  }
 
 end Over
 
@@ -102,14 +100,70 @@ theorem not_elim (t : ¬s.Terminal) : ¬s.Closed ∨ (∃ g, s.NextTag g) := by
 end Terminal
 end State
 
+variable [Practical α] {rtr : α}
+
+namespace Instantaneous
+namespace Step
+
+variable {s₁ s₂ : State α}
+
+theorem of_acyclic_not_closed {s : State.Over rtr} (a : Dependency.Acyclic rtr) (hc : ¬s.Closed) : 
+    ∃ s' : State α, Nonempty (s ⇓ᵢ s') := 
+  have := s.not_closed_to_nontrivial hc
+  let ⟨rcn, ha⟩ := s.allowed_of_acyclic_not_closed a hc
+  if h : s.Triggers rcn
+  then ⟨s.exec rcn |>.record rcn, ⟨.exec ha h⟩⟩
+  else ⟨s.record rcn, ⟨.skip ha h⟩⟩  
+
+theorem unprocessed₂_eq (e : s₁ ⇓ᵢ s₂) : s₂.unprocessed = s₁.unprocessed \ {e.rcn} := by
+  ext i
+  simp [State.unprocessed, Equivalent.obj?_rcn_eq e.equiv, and_assoc]
+  intro _
+  have h := e.mem_progress_iff (rcn' := i) |>.not
+  push_neg at h
+  simp [and_comm, h]
+  
+theorem unprocessed₁_eq (e : s₁ ⇓ᵢ s₂) : s₁.unprocessed = insert e.rcn s₂.unprocessed := by
+  sorry
+
+end Step
+
+theorem ClosedExecution.of_nonterminal_acyclic_not_closed {s : State.Over rtr} 
+    (ht : ¬s.Terminal) (a : Dependency.Acyclic rtr) (hc : ¬s.Closed) : ∃ s' : State α, Nonempty (s ⇓| s') := by
+  generalize hp : s.unprocessed = u
+  have hu := hp ▸ s.unprocessed_finite
+  induction u, hu using Set.Finite.dinduction_on generalizing s rtr
+  case _ =>
+    exact absurd (State.Over.unprocessed_empty_iff_closed.mp hp) hc
+  case _ rcn r u hr hi =>
+    have ⟨sₑ, ⟨e⟩⟩ := Instantaneous.Step.of_acyclic_not_closed a hc
+    by_cases hc' : sₑ.Closed
+    case pos => exact ⟨_, ⟨.single e, hc'⟩⟩ 
+    case neg =>
+      suffices hs : (State.Over.ofStep e).unprocessed = r by
+        have a' := Dependency.Acyclic.equiv e.equiv (s.rtr_eq.symm ▸ a)
+        have ht' := State.Terminal.not_of_not_closed hc'
+        have ⟨_, ⟨e'⟩⟩ := hi (s := State.Over.ofStep e) ht' a' hc' hs
+        exact ⟨_, ⟨.trans e e'.exec, e'.closed⟩⟩ 
+      -- Is this provable, or do we need to adjust the way we do induction here?
+      clear hi ht hc a hc'
+      simp [State.Over.ofStep]
+      -- rw [State.Over.ofStep, e.unprocessed₂_eq, hp, Set.insert_diff_self_of_not_mem u]
+      sorry 
+
+end Instantaneous
+
+theorem AdvanceTag.of_nonterminal_closed {s : State.Over rtr} 
+    (ht : ¬s.Terminal) (hc : s.Closed) : ∃ s' : State α, Nonempty (s ⇓- s') := by
+  have ⟨g, hg⟩ := State.Terminal.not_elim ht |>.resolve_left (not_not.mpr hc)
+  exact ⟨⟨clear s.rtr, g, ∅⟩, ⟨hc, ⟨hg, clear_cleared _⟩⟩⟩
+
 -- A reactor has the progress property, if from any nonterminal state based at that reactor, we can 
 -- perform an execution step.
 def Progress [Practical α] (rtr : α) : Prop :=
   ∀ (s : State.Over rtr), ¬s.Terminal → (∃ s' : State α, s ⇓ s')    
 
 namespace Progress
-
-variable [Practical α] {rtr : α} in section
 
 theorem to_deps_acyclic_nontriv (nontriv : rtr[.rcn].Nonempty) (p : Progress rtr) : 
     Dependency.Acyclic rtr := by
@@ -127,37 +181,14 @@ theorem to_deps_acyclic : (Progress rtr) → Dependency.Acyclic rtr :=
   then to_deps_acyclic_nontriv h 
   else fun _ => Dependency.Acyclic.of_trivial (Partial.Nonempty.not_to_empty h)
 
-end
-
-variable [Practical α] {rtr : α}
-
-theorem Instantaneous.Step.of_acyclic_not_closed {s : State.Over rtr} 
-    (a : Dependency.Acyclic rtr) (hc : ¬s.Closed) : ∃ s' : State α, Nonempty (s ⇓ᵢ s') := 
-  have := s.not_closed_to_nontrivial hc
-  let ⟨rcn, ha⟩ := s.allowed_of_acyclic_not_closed a hc
-  if h : s.Triggers rcn
-  then ⟨s.exec rcn |>.record rcn, ⟨.exec ha h⟩⟩
-  else ⟨s.record rcn, ⟨.skip ha h⟩⟩  
-
 theorem of_deps_acyclic (a : Dependency.Acyclic rtr) : Progress rtr := by
   intro s ht
-  by_cases hc : s.Closed
-  case pos =>
-    have ⟨g, hg⟩ := State.Terminal.not_elim ht |>.resolve_left (not_not.mpr hc)
-    exact ⟨⟨clear s.rtr, g, ∅⟩, .advance ⟨hc, ⟨hg, clear_cleared _⟩⟩⟩
-  case neg =>
-    generalize hp : s.unprocessed' = u
-    induction u using Finset.induction_on generalizing s rtr -- TODO: We might need Finset.induction_on'
-    case empty => exact absurd (State.Over.unprocessed'_empty_iff_closed.mp hp) hc
-    case insert r u hr hi =>
-      have ⟨sₑ, ⟨e⟩⟩  := Instantaneous.Step.of_acyclic_not_closed a hc
-      by_cases hc' : sₑ.Closed
-      case pos => exact ⟨_, .close ⟨.single e, hc'⟩⟩ 
-      case neg =>
-        let sₑ : State.Over sₑ.rtr := { sₑ with progress_sub := sorry }
-        have ⟨_, e'⟩ := hi sorry sₑ sorry hc' sorry
-        have ⟨e'⟩ := e'.not_closed_elim hc'
-        exact ⟨_, .close $ ⟨.trans e e'.exec, sorry⟩⟩ 
+  if hc : s.Closed then 
+    have ⟨_, ⟨a⟩⟩ := AdvanceTag.of_nonterminal_closed ht hc
+    exact ⟨_, .advance a⟩
+  else 
+    have ⟨_, ⟨e⟩⟩ := Instantaneous.ClosedExecution.of_nonterminal_acyclic_not_closed ht a hc
+    exact ⟨_, .close e⟩
 
 theorem iff_deps_acyclic : (Progress rtr) ↔ (Dependency.Acyclic rtr) :=
   ⟨to_deps_acyclic, of_deps_acyclic⟩
