8410e005a4c1a70acd8fdf8433eae01dcfa730a3
Marcus Rossel <marcusrossel@icloud.com>
2023-05-22 16:17:35 +0200

    Update Lean/Mathlib version

--- src/lakefile.lean
+++ src/lakefile.lean
@@ -3,4 +3,4 @@ open Lake DSL
 
 package reactor_model
 lean_lib ReactorModel
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"b237400a22f07afaf1"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"c6afc89a90f7d587030"
