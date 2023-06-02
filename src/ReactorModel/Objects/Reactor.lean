3a264f72cdcf9cc0b86f36289708f4806f2bb684
Marcus Rossel <marcusrossel@icloud.com>
2023-06-02 11:47:59 +0200

    Remove Objects/Reactor/Instances

--- src/ReactorModel/Objects/Reactor.lean
+++ src/ReactorModel/Objects/Reactor.lean
@@ -6,6 +6,3 @@ import ReactorModel.Objects.Reactor.Proper
 import ReactorModel.Objects.Reactor.Theorems
 import ReactorModel.Objects.Reactor.Updatable
 import ReactorModel.Objects.Reactor.WellFounded
--- import ReactorModel.Objects.Reactor.Instances.Basic
--- import ReactorModel.Objects.Reactor.Instances.Core
--- import ReactorModel.Objects.Reactor.Instances.Raw
